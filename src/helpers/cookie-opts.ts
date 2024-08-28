ed18c46aee758f7f9db79e991f5de825cef18997
Ivan <lavet13@mail.ru>
2024-08-28 16:17:36 +0300

    just for now

--- src/helpers/cookie-opts.ts
+++ src/helpers/cookie-opts.ts
@@ -3,7 +3,7 @@ import { CookieListItem } from "@whatwg-node/cookie-store";
 export const cookieOpts: CookieListItem = {
   httpOnly: true,
   sameSite: 'lax',
-  secure: true,
+  secure: false,
   path: '/',
   domain: null,
   expires: null,
